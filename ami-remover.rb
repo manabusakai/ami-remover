59348be54d1f3e84f6dc8c88711c60a9e0aa7cf3
Manabu Sakai <rea.527@gmail.com>
2017-09-20 07:07:24 +0900

    Get list of AMI

---
+++ ami-remover.rb
@@ -0,0 +1,45 @@
+require 'aws-sdk-ec2'
+
+AWS_REGION='ap-northeast-1'
+
+def get_snapshot_ids(block_device_mappings)
+  snapshot_ids = []
+  block_device_mappings.each do |mapping|
+    snapshot_ids.push mapping.ebs.snapshot_id
+  end
+  return snapshot_ids
+end
+
+def get_images
+  ec2  = Aws::EC2::Client.new(region: AWS_REGION)
+  resp = ec2.describe_images({owners: ['self']})
+
+  images = {}
+
+  resp.images.each do |image|
+    image_id      = image.image_id
+    name          = image.name
+    creation_date = image.creation_date
+    snapshot_ids  = get_snapshot_ids(image.block_device_mappings)
+
+    images[image_id] = {
+      'name'          => name,
+      'creation_date' => creation_date,
+      'snapshot_id'   => snapshot_ids,
+    }
+  end
+
+  return images
+end
+
+def describe_images
+  puts "IMAGE_ID\tNAME\tCREATION_DATE"
+
+  get_images.each do |image_id, attribute|
+    print "#{image_id}\t"
+    print "\"#{attribute['name']}\"\t"
+    print "\"#{attribute['creation_date']}\"\n"
+  end
+end
+
+describe_images
